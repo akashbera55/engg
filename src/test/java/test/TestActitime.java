041fb114d316b5b2d16d9ff6e5de1cfd80f93054
USER <USER@DESKTOP-QBFP0JU>
2020-12-08 17:44:02 +0530

    Commit Completed

---
+++ src/test/java/test/TestActitime.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import generics.BaseTest;
+import pom.PomPro;
+
+
+public class TestActitime extends BaseTest
+{
+	@Test
+	public void login() throws IOException
+	{
+		PomPro l=new PomPro(driver);
+		l.loginMethod();
+	}
+}
